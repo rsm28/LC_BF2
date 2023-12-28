f7f2dae100c4c1f2f4a96be0b95f525723b9e2db
oanon-ak <91552832+rsm28@users.noreply.github.com>
2023-12-28 04:00:50 +0000

    pushaaaaa

--- run.bat
+++ run.bat
@@ -1,3 +1,15 @@
 @echo off
-python downloader.py
+IF EXIST downloader.py (
+    echo downloader.py exists, running the script...
+    python downloader.py
+) ELSE (
+    echo downloader.py not found, attempting to download...
+    powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rsm28/LC_BF2/main/downloader.py' -OutFile 'downloader.py'"
+    IF EXIST downloader.py (
+        echo Successfully downloaded downloader.py, running the script...
+        python downloader.py
+    ) ELSE (
+        echo Failed to download downloader.py. Please check your internet connectivity or the URL and try again.
+    )
+)
 pause
