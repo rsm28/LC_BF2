90570138487072a1e691add957569f7318085229
oanon-ak <91552832+rsm28@users.noreply.github.com>
2023-12-28 03:55:10 +0000

    DEBUGUGUGUGUG

--- downloader.py
+++ downloader.py
@@ -9,17 +9,20 @@ MOD_DOWNLOAD_PATH = "mod_downloads"
 
 def clear_mod_downloads():
     print("Clearing everything from " + MOD_DOWNLOAD_PATH + "...")
-    # Check if MOD_DOWNLOAD_PATH exists
     if os.path.exists(MOD_DOWNLOAD_PATH):
         for root, dirs, files in os.walk(MOD_DOWNLOAD_PATH):
             for f in files:
                 os.unlink(os.path.join(root, f))
+                print(f"Deleted file: {f}")
             for d in dirs:
                 shutil.rmtree(os.path.join(root, d))
+                print(f"Deleted directory: {d}")
+        print("Mod downloads cleared.")
     else:
         print("MOD_DOWNLOAD_PATH does not exist, skipping")
 
 def clear_old_bepinex():
+    print("Clearing old BepInEx files...")
     files = ["doorstop_config.ini", "winhttp.dll"]
     directories = ["BepInEx"]
 
@@ -31,27 +34,31 @@ def clear_old_bepinex():
         if os.path.isdir(directory):
             shutil.rmtree(directory)
             print(f"Successfully removed the directory {directory}")
+    print("Old BepInEx files cleared.")
 
 def download_mod(author, mod, version):
+    print(f"Preparing to download {mod} version {version} from author {author}...")
     if mod == "LC_API":
         version = "2.2.0"
-    print(f"Downloading {mod} version {version} from author {author}...")
-    
+        print(f"Version override: Downloading {mod} version {version} instead.")
+
     webrequest_url = f"https://thunderstore.io/package/download/{author}/{mod}/{version}/"
-    print(webrequest_url)
-    
+    print(f"Downloading from URL: {webrequest_url}")
+
     response = requests.get(webrequest_url)
     download_path = f"{MOD_DOWNLOAD_PATH}/{mod}_{version}.zip"
     with open(download_path, 'wb') as f:
         f.write(response.content)
+    print(f"Download complete: {mod}_{version}.zip")
 
     print(f"Extracting {mod} version {version}...")
     with zipfile.ZipFile(download_path, 'r') as zip_ref:
         zip_ref.extractall(f"{MOD_DOWNLOAD_PATH}/")
+    print(f"Extraction complete: {mod} version {version}")
     clear_mod_downloads()
     
-
 def process_modpack(modpack):
+    print(f"Processing modpack: {modpack}")
     authors = []
     mods = []
     versions = []
@@ -66,51 +73,53 @@ def process_modpack(modpack):
         return
     for i in range(len(authors)):
         download_mod(authors[i], mods[i], versions[i])
+    print(f"Modpack {modpack} processed successfully.")
 
 def download_and_setup_bepinex():
     print("Downloading and extracting BepInExPack...")
-    # Step 1: Download BepInExPack
     response = requests.get('https://thunderstore.io/package/download/BepInEx/BepInExPack/5.4.2100/')
     download_path = "BepInExPack_5.4.2100.zip"
     with open(download_path, 'wb') as f:
         f.write(response.content)
+    print("BepInExPack downloaded.")
 
-    # Step 2: Extract the zip file directly to the root directory
+    print("Extracting BepInExPack...")
     with zipfile.ZipFile(download_path, 'r') as zip_ref:
         zip_ref.extractall(".")
+    print("BepInExPack extracted.")
 
     print("Launching LC to install BepInEx...")
-    # Step 4: Launch and close Lethal Company.exe
     process = subprocess.Popen(["Lethal Company.exe"])
     time.sleep(5)
     process.terminate()
+    print("LC launched and terminated to install BepInEx.")
 
-    if os.path.isfile("BepInExPack_5.4.2100.zip"):
-        os.remove("BepInExPack_5.4.2100.zip")
+    if os.path.isfile(download_path):
+        os.remove(download_path)
+        print(f"Cleanup: Removed {download_path}")
 
 def fetch_modpacks():
+    print("Fetching list of modpacks from GitHub...")
     user = 'rsm28'
     repo = 'LC_BF2'
     response = requests.get(f"https://api.github.com/repos/{user}/{repo}/contents/modpacks")
-    response.raise_for_status()  # Ensure we got a successful response
+    response.raise_for_status()
 
     modpacks = [item['name'].replace('.txt', '') for item in response.json()]
-
-    print("----------------------- MODPACKS -----------------------")
+    print("Available modpacks:")
     for i, modpack in enumerate(modpacks, start=1):
         print(f"{i}) {modpack}")
-    print("----------------------- MODPACKS -----------------------")
 
     modpack = input("Enter the EXACT NAME of the modpack you want to install: ")
-
     if modpack in modpacks:
         print(f"Downloading {modpack}...")
         modpack_url = f"https://raw.githubusercontent.com/{user}/{repo}/main/modpacks/{modpack}.txt"
         response = requests.get(modpack_url)
-        response.raise_for_status()  # Ensure we got a successful response
+        response.raise_for_status()
 
         with open(f"./modpacks/{modpack}.txt", 'w') as f:
             f.write(response.text)
+        print(f"{modpack} downloaded successfully.")
     else:
         print(f"Modpack '{modpack}' does not exist. Exiting...")
         exit(1)
